70ecd66aab6f207716b614f92803831b76fbc937
Kirill Shikov <rbhz2806@mail.ru>
2026-07-01 11:16:56 +0300

    исправление замечаний

--- src/common/interceptors/transform.interceptor.ts
+++ src/common/interceptors/transform.interceptor.ts
@@ -1,6 +1,6 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, catchError } from 'rxjs/operators';
 
 export interface Response<T> {
   status: 'success';
@@ -12,9 +12,12 @@ export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> 
   intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
     return next.handle().pipe(
       map(data => ({
-        status: 'success',
+        status: 'success' as const,
         data,
       })),
+      catchError(error => {
+        throw error;
+      }),
     );
   }
 }
